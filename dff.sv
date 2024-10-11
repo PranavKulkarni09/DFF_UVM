e927a7d49d27fc77a9b2c42e0ad4cbe4dbcac2ac
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2024-10-10 19:42:54 -0500

    Create dff.sv

---
+++ dff.sv
@@ -0,0 +1,15 @@
+module dff(clk, rst, din, dout);
+  input clk, rst, din;
+  output reg dout;
+  always@(posedge clk)
+    begin
+      if(rst)
+        dout<=0;
+      else
+        dout<=din;
+    end
+endmodule
+
+interface dff_if;
+  logic clk, rst, din, dout;
+endinterface
